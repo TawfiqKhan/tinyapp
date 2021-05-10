63b92fdb00b678becc2e1ebc6f5eff0f4337da18
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-10 16:12:50 -0400

    individual urls working

--- server.js
+++ server.js
@@ -10,10 +10,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get('/urls/:urlId', (req, res)=> {
+  console.log(req.params.id)
+  let shortURL = req.params.urlId;
+  let longURL = urlDatabase[shortURL];
+  console.log(shortURL, longURL)
+  const templateVars = {shortURL, longURL}
+  console.log(templateVars);
+  res.render("urls_show", templateVars)
+})
+
 app.get('/urls', (req, res)=> {
   const templateVars = {urls: urlDatabase}
   res.render("urls_index", templateVars)
 })
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
