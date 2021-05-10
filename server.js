8de3dc65839ee8ec4263d65e05e0f04ff86e3417
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-10 18:06:54 -0400

    function to generate random string added

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs")
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/urls/:urlId', (req, res)=> {
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
+app.get('/urls/:urlId', (req, res) => {
   let shortURL = req.params.urlId;
   let longURL = urlDatabase[shortURL];
-  const templateVars = {shortURL, longURL}
+  const templateVars = { shortURL, longURL }
   res.render("urls_show", templateVars)
 })
 
-app.get('/urls', (req, res)=> {
-  const templateVars = {urls: urlDatabase}
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars)
 })
 
@@ -43,3 +52,14 @@ app.get("/fetch", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is Listening to http://localhost:${PORT}`)
 })
+
+//Helper function
+
+function generateRandomString() {
+  let output = ''
+  let charSet = 'abcdefghijklmnopqrstuvwxyz123456789'
+  for (let i = 0; i < 7; i++) {
+    output = output.concat(charSet[Math.floor(Math.random() * charSet.length - 1)])
+  }
+  return output;
+}
